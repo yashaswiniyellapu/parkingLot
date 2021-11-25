6f78963022ef0f28e3e8e4df3a1c8d10233ad870
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-11-25 16:38:37 +0530

    Created vehicle Interface.

---
+++ src/main/java/com/everestengineering/servicetype/Vehicle.java
@@ -0,0 +1,8 @@
+package com.everestengineering.servicetype;
+
+
+import com.everestengineering.model.SlotType;
+
+public interface VehicleInterface {
+    void registerData( String regNo,String color);
+}
--- src/main/java/com/everestengineering/servicetype/VehicleInterface.java
+++
@@ -1,2 +0,0 @@
-package com.everestengineering.servicetype;public interface VehicleInterface {
-}
