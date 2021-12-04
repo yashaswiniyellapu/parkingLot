9fc465f092d6a6bb0dccffda188bcc5cfeb78ba6
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-04 19:19:54 +0530

    created floor

--- src/main/java/com/everestengineering/Floor.java
+++ src/main/java/com/everestengineering/Floor.java
@@ -4,26 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Floor {
+    private final int numOfSlots;
     private List<Slot> spaceList = new ArrayList<>();
-    public void createTruckSpace()
-    {
-        for(int i=0; i<1; i++)
-        {
-            spaceList.add(new Slot(SlotType.TRUCK,"unparked"));
+
+    Floor(int numOfSlots) {
+        this.numOfSlots = numOfSlots;
+        createTruckSpace();
+        createBikeSpace();
+        createCarSpace();
+    }
+
+    public void createTruckSpace() {
+        for (int i = 0; i < 1; i++) {
+            spaceList.add(new Slot(SlotType.TRUCK, "unparked"));
         }
     }
-    public void createBikeSpace()
-    {
-        for(int i=1; i<=2; i++)
-        {
-            spaceList.add(new Slot(SlotType.TRUCK,"unparked"));
+
+    public void createBikeSpace() {
+        for (int i = 1; i <= 2; i++) {
+            spaceList.add(new Slot(SlotType.TRUCK, "unparked"));
         }
     }
-    public void createCarSpace()
-    {
-        for(int i=3; i<numOfSlots; i++)
-        {
-            spaceList.add(new Slot(SlotType.TRUCK,"unparked"));
+
+    public void createCarSpace() {
+        for (int i = 3; i < numOfSlots; i++) {
+            spaceList.add(new Slot(SlotType.TRUCK, "unparked"));
         }
     }
 }
