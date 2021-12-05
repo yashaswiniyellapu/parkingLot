3dca5884a809ec690950996000e4be87d43df420
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-05 16:31:04 +0530

    extended truck

---
+++ src/main/java/com/everestengineering/Truck.java
@@ -0,0 +1,7 @@
+package com.everestengineering;
+
+public class Truck extends Vehicle {
+    protected Truck(String registrationId, String color, SlotType slotType) {
+        super(registrationId, color, slotType);
+    }
+}
