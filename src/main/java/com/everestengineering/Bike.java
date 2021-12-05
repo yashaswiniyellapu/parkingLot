bb8199c867765c1bb13d4c1199b08a47411b37ee
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-05 16:31:24 +0530

    extended bike

---
+++ src/main/java/com/everestengineering/Bike.java
@@ -0,0 +1,7 @@
+package com.everestengineering;
+
+public class Bike extends Vehicle {
+    protected Bike(String registrationId, String color, SlotType slotType) {
+        super(registrationId, color, slotType);
+    }
+}
