c299d2c32c8ea39182669b4a88ddba1c9c306e8d
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-05 16:31:40 +0530

    extended car

---
+++ src/main/java/com/everestengineering/Car.java
@@ -0,0 +1,7 @@
+package com.everestengineering;
+
+public class Car extends Vehicle{
+    protected Car(String registrationId, String color, SlotType slotType) {
+        super(registrationId, color, slotType);
+    }
+}
