9d97d100857024262894201a82d92f3200f89d35
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-05 16:32:20 +0530

    parked and unparked vehicle

--- src/main/java/com/everestengineering/ParkingLot.java
+++ src/main/java/com/everestengineering/ParkingLot.java
@@ -1,19 +1,47 @@
 package com.everestengineering;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
     private String parklotId;
     private int numOfFloors;
     private int numOfSlots;
+    static List<List<Slot>> floorSpace = new ArrayList<>();
 
     public ParkingLot(String parklotId, int numOfFloors, int numOfSlots) {
         this.parklotId = parklotId;
         this.numOfFloors = numOfFloors;
         this.numOfSlots = numOfSlots;
+        build();
     }
 
     public void build() {
         for (int i = 0; i < numOfFloors; i++) {
-            new Floor(numOfSlots);
+            Floor floor = new Floor(numOfSlots);
+            floorSpace.add(i, floor.getSpaceList());
         }
     }
+
+    public void park(Vehicle vehicle) {
+        ValidateSpace validateSpace = new ValidateSpace(vehicle.getSlotType());
+        boolean checkFloor = validateSpace.checkFloor();
+        if (checkFloor) {
+            int[] position = validateSpace.getPosition();
+            System.out.println("Parked vehicle. TicketId " + parklotId + "_" + position[0] + "_" + position[1]);
+            floorSpace.get(position[0]).get(position[1]).setStatus(vehicle.getRegistrationId() + " " + vehicle.getColor());
+        }
+        if (!checkFloor) {
+            System.out.println("Parklot is full");
+        }
+
+    }
+
+    public void unPark(String ticketId) {
+        String[] ticket = ticketId.split("_");
+        String[] data = floorSpace.get(Integer.parseInt(ticket[1])).get(Integer.parseInt(ticket[2])).getStatus().split(" ");
+        System.out.println("Unparked vehicle with Registration number " + data[0] + " and color " + data[1]);
+        floorSpace.get(Integer.parseInt(ticket[1])).get(Integer.parseInt(ticket[2])).setStatus("unparked");
+    }
+
 }
